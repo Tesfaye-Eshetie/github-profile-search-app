0113162e9390c71f3cc592e6b12b95dda4f05c8a
tesfaye eshetie <tesfayeeshetie19@gmail.com>
2022-03-04 17:39:24 -0500

    primary search app and profile display card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState({});
+  var [ userName, setUserName ] = useState("");
+
+  const handleChange = (e) => {
+      setUserName(e.target.value);
+  }
+
+  const githubURL = `https://api.github.com/users/${userName}`;
+  
+  useEffect(() => {
+      const fetchData = async () => {
+        try {
+        const { data } = await axios.get(githubURL);
+        setUser(data);
+        } catch (err) {
+          console.log(err);
+        }
+    }
+      fetchData();
+    }, [ userName ]);
+
+  const userID = user.name || user.login
+  const userBio = user.bio ? user.bio : ''
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="user-form"  >
+        <input type="text" placeholder="Search for a Github User ..." onChange={handleChange} />
+      </form>
+      <main>
+        {userID &&
+          <div className="card">
+            <div>
+              <img src={user.avatar_url} alt={user.name} className="avatar" />
+            </div>
+            <div className="user-info">
+              <h2>{userID}</h2>
+                <p>{userBio}</p>
+              <ul>
+                <li>{user.followers} <strong>Followers</strong></li>
+                <li>{user.following} <strong>Following</strong></li>
+                <li>{user.public_repos} <strong>Repos</strong></li>
+              </ul>
+            </div>
+          </div>
+        }
+      </main>
     </div>
   );
 }
